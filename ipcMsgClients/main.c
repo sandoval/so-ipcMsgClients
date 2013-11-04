cf642bb551bce0d6ebb253a064dc88f312aab731
Daniel Sandoval <daniel@loopec.com.br>
2013-11-04 21:54:49 -0200

    Initial Commit

---
+++ ipcMsgClients/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ipcMsgClients
+//
+//  Created by Daniel Sandoval on 04/11/2013.
+//  Copyright (c) 2013 Departamento de Ciência da Computação - Universidade de Brasília. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
