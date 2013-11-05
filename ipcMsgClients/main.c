930da82cba20a4af53b199b510fc917f16d397c5
Daniel Sandoval <daniel@loopec.com.br>
2013-11-04 22:11:58 -0200

    Initialized project. Uses library to perform simple message sending test. All nodes initialize and send a message to node zero.

--- ipcMsgClients/main.c
+++ ipcMsgClients/main.c
@@ -7,12 +7,51 @@
 //
 
 #include <stdio.h>
+#include "../../ipcMessaging/ipcMessaging/ipcMessaging.h"
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+void treatSigint() {
+    printf("Received SIGINT!\n");
+    tearDown();
+    exit(1);
+}
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    int pid, i;
+    message message;
+    struct sigaction action;
+    signal(SIGCHLD, SIG_IGN);
+    
+    for (i = 0; i < 8; i++) {
+        pid = fork();
+        if (pid == 0) {
+            setup(i);
+            break;
+        }
+    }
+    if (i == 8) {
+        setup(i);
+    }
+    
+    action.sa_handler = treatSigint;
+    sigaction(SIGINT, &action, NULL);
+    
+    message.destination = 0;
+    message.source = i;
+    message.mtype = 1;
+    strcpy(message.text, "Brubles!");
+    sendMessage(&message);
+    
+    printf("Node %d reporting!\n", i);
+    for (i = 0; i<10; i++) {
+        sleep(10);
+    }
+    
+    tearDown();
+    
     return 0;
 }
-
